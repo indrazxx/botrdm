91fdcfa1d22c6655688872990ecbb3f91f796e20
indrazxx <42199565+indrazxx@users.noreply.github.com>
2018-08-08 13:54:49 +0700

    Add files via upload

---
+++ Kalkulator-Bot-LINE-master/config.php
@@ -0,0 +1,5 @@
+<?php
+
+$channelAccessToken = 'RE+C+5L3qehUVpdAfh7KMLyWPjnJijvgSkob+Qgm1u/hCYox97I2C9WuAbTOYUeVh+w1sIoxtr5p6PhkFRp2O4l+dXbLWLv5thga/0m5f/69MuXIri1oxt1cz8sDkyaXG6YLalfJQ4V5xkIDhertqwdB04t89/1O/w1cDnyilFU=';
+
+$channelSecret = 'dfadfa548b120de779ebdb24338ac973';
